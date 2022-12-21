699c8f27fe5750e6a0fc27b6f1eea281aec5ea81
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2022-12-21 21:18:29 +0700

    new

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/api/messages", messageRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    // origin: "https://localhost:8080",
+    origin: "https://chat-app-front-one.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
